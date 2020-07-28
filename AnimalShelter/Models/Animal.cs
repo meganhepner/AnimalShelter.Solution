16ddf393fd7230c26f607bff6a1c688b2f7ddb8d
Megan Hepner <megan.hepner@gmail.com>
2020-07-28 15:16:08 -0700

    work with date input
    Co-authored-by: Taylor Somers <tsomers@yandex.com>

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -7,5 +7,10 @@ namespace AnimalShelter.Models
     public string Gender { get; set; }
     public string DateOfAdmittance { get; set; }
     public string Type { get; set; }
+
+    public struct DateTime : IComparable, IFormattable, IConvertible, ISerializable, IComparable<DateTime>, IEquatable<DateTime>  
+    {  
+     // Contains methods and properties  
+    }
   }
 }
