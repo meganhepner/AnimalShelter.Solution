e33121f7098c90c8f20c32672950646acfd97e56
Megan Hepner <megan.hepner@gmail.com>
2020-07-28 15:53:15 -0700

    complete Create form to take animal input
    Co-authored-by: Taylor Somers <tsomers@yandex.com>

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,3 +1,6 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelter.Models
 {
   public class Animal
@@ -5,12 +8,12 @@ namespace AnimalShelter.Models
     public int AnimalId { get; set; }
     public string Name { get; set; }
     public string Gender { get; set; }
-    public string DateOfAdmittance { get; set; }
+    // public string DateOfAdmittance { get; set; }
     public string Type { get; set; }
+    public string Breed { get; set; }
 
-    public struct DateTime : IComparable, IFormattable, IConvertible, ISerializable, IComparable<DateTime>, IEquatable<DateTime>  
-    {  
-     // Contains methods and properties  
-    }
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    public DateTime DateOfAdmittance{ get; set; }
   }
 }
